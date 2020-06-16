8b668d3964c7f04ca3fb20625cfdb80fa4c9f7ec
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-06-16 12:26:22 -0700

    cleaan up clang file

--- arch/clang.sh
+++ arch/clang.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
-# -DTIME
-# -DINT_TIME
-
-#CXXFLAGS=-flto
 TOOLCHAIN_FILE=/home/jgwohlbier/DSSoC/DASH/TraceAtlas/vcpkg/scripts/buildsystems/vcpkg.cmake
-#LDFLAGS=-fuse-ld=lld
 
-CC=`which clang` \
-CXX=`which clang++` \
 cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} \
